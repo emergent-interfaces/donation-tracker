a2000012499026cabead3a33b3a0d32f74a54860
superlou <lousimons@gmail.com>
2012-10-21 19:41:19 -0400

    Adding Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DonationTracker::Application.config.secret_token = 'c14ed84e04905b3c84d8fa20141a802679c7d1c24d4c9430670dd8f8b276b1143a24e165c78de3879874fc73a7394b9e211846d3c8d1385e46097fb7bbaa1c6a'
